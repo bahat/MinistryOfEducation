594aeea597d31a1b5a487e8baa487341a81bc156
Omer Bahat <bahat.omer@gmail.com>
2017-12-13 14:32:12 +0200

    More efficient Mail sending

--- DepartmentOfEducation/AddStudents.aspx.cs
+++ DepartmentOfEducation/AddStudents.aspx.cs
@@ -17,6 +17,7 @@ public partial class AddStudents : System.Web.UI.Page
 
     protected void Insert_Click(object sender, EventArgs e)
     {
+        SendItReference.DepartmentOfEducation Client = new SendItReference.DepartmentOfEducation();
         int id=0;
         SqlConnection c = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Omer\\Dropbox\\school\\UpGradeProject\\DepartmentOfEducation\\App_Data\\Students.mdf;Integrated Security=True");
         SqlCommand cmd = new SqlCommand("INSERT INTO Students (DOB, FirstName, LastName, SchoolID, PhoneNumber, MailAddress) VALUES(@DOB, @FirstName, @LastName, @SchoolID, @PhoneNumber, @MailAddress); SELECT StudentId From Students WHERE StudentId = SCOPE_IDENTITY()", c);
@@ -43,62 +44,5 @@ public partial class AddStudents : System.Web.UI.Page
     }
 
     
-    public void sendit(int id)
-    {
-        string ReciverMail;
-        SqlConnection c = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Omer\\Dropbox\\school\\UpGradeProject\\DepartmentOfEducation\\App_Data\\Students.mdf;Integrated Security=True");
-        SqlCommand cmd = new SqlCommand("SELECT MailAddress FROM Students Where StudentId = @ID", c);
-        cmd.Parameters.AddWithValue("ID", id);
-        c.Open();
-        cmd.ExecuteNonQuery();
-        c.Close();
-        try
-        {
-            c.Open();
-            ReciverMail = (string)cmd.ExecuteScalar();
-            c.Close();
-            //Response.Redirect("http://www.google.com");
-        }
-        catch
-        {
-
-            ReciverMail = "No mail Detected";
-        }
-
-
-        MailMessage msg = new MailMessage();
-        msg.From = new MailAddress("Support@UpGrade.com");
-        if (ReciverMail == "No mail Detected")
-            Response.Redirect("http://NOMAILDETECTED.com");
-        else
-        {
-            msg.To.Add(ReciverMail);
-            msg.Subject = "SignUpInfo";
-            msg.Body = "Hello, welcome to your Account info,\nYour ID is "+id+" and your temporary password is also "+id+ ".\n Your account type is Student so make sure you select it at login page.";
-            SmtpClient client = new SmtpClient();
-            client.UseDefaultCredentials = true;
-            client.Host = "smtp.gmail.com";
-            client.Port = 587;
-            client.EnableSsl = true;
-            client.DeliveryMethod = SmtpDeliveryMethod.Network;
-            client.Credentials = new NetworkCredential("Sup.UpGrade@gmail.com", "Arik2005");
-            client.Timeout = 20000;
-            try
-            {
-                client.Send(msg);
-                Response.Redirect("https://www.google.com");//return success
-            }
-            catch (Exception ex)
-            {
-                Response.Redirect("https://www.bing.com");//return fail
-            }
-            finally
-            {
-                msg.Dispose();
-            }
-        }
-
-
-}
 
 }
